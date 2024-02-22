7a945d3b523dc260d3aebc34931ea586276eff82
TSARBOMBA <dicoud@gmail.com>
2024-02-22 20:41:21 +0100

    initial commit

---
+++ reack-app/src/components/ListGroup.tsx
@@ -0,0 +1,24 @@
+function ListGroup() {
+  let items = [
+    "New York",
+    "San Francicsco",
+    "Tokyo",
+    "Warsaw",
+    "London",
+    "Paris",
+  ];
+  items = [];
+
+  return (
+    <>
+      <h1>List</h1>
+      {items.length === 0 && <p>No item found</p>}
+      <ul className="list-group">
+        {items.map((item) => (
+          <li key={item}>{item}</li>
+        ))}
+      </ul>
+    </>
+  );
+}
+export default ListGroup;
