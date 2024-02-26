75521cdfabff5c5ace75d1ec284fd39ddee3d080
TSARBOMBA <dicoud@gmail.com>
2024-02-26 23:06:04 +0100

    in poggers

--- reack-app/src/components/ListGroup.tsx
+++ reack-app/src/components/ListGroup.tsx
@@ -1,4 +1,4 @@
-import { MouseEvent } from "react";
+import { useState } from "react";
 
 function ListGroup() {
   let items = [
@@ -9,8 +9,9 @@ function ListGroup() {
     "London",
     "Paris",
   ];
+  const [selectedIndex, setSelectedIndex]= useState(-1);
 
-  const handleClick = (event: MouseEvent) => console.log(event);
+
 
   return (
     <>
@@ -18,7 +19,7 @@ function ListGroup() {
       {items.length === 0 && <p>No item found</p>}
       <ul className="list-group">
         {items.map((item, index) => (
-          <li className="list-group-item" key={item} onClick={handleClick}>{item}</li>
+          <li className={selectedIndex === index ? 'list-group-item active' : 'list-group-item'} key={item} onClick={() => setSelectedIndex(index)}>{item}</li>
         ))}
       </ul>
     </>
