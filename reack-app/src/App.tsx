f0ef7b1375a052a90905aece94cad10c14a156ad
TSARBOMBA <dicoud@gmail.com>
2024-02-28 23:50:19 +0100

    finish pt1

--- reack-app/src/App.tsx
+++ reack-app/src/App.tsx
@@ -1,7 +1,19 @@
-import ListGroup from "./components/ListGroup";
+import { useState } from "react";
+import Alert from "./components/Alert";
+import Button from "./components/Button";
 
 function App() {
-  return <div><ListGroup /></div>;
+  const [AlertVisible, setAlertVisibility] = useState(false);
+  return (
+    <div>
+      {AlertVisible && (
+        <Alert onClose={() => setAlertVisibility(false)}>My alert</Alert>
+      )}
+      <Button color="secondary" onClick={() => setAlertVisibility(true)}>
+        My button
+      </Button>
+    </div>
+  );
 }
 
 export default App;
---
+++ reack-app/src/components/Button.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+interface Props {
+  children: string;
+  onClick: () => void;
+  color?: "primary" | "secondary";
+}
+
+const Button = ({ children, onClick, color = "primary" }: Props) => {
+  return (
+    <button className={"btn btn-" + color} onClick={onClick} color="">
+      {children}
+    </button>
+  );
+};
+
+export default Button;
